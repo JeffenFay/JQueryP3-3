a1084002e1ad0b4059d87316e08fbc975d5a0d55
JeffenFay <jeffen.fay@gmail.com>
2018-10-16 13:39:15 +0200

    Controle de saisie

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 var guessMe = Math.round(Math.random()*100);
-alert(guessMe);
+var nbrClick = 0;
+var regex = /^[0-9]{1,3}$/;
+// alert(guessMe);
 $('button').click(function(){
-  if ($('#valueGuess').val() > guessMe) {
-    $('#guessed').css('background-color','red');
+  nbrClick+=1;
+  if(!regex.test($('#valueGuess').val())||$('#valueGuess').val()>100){
+    $('#guessed').css('background-color','black');
+    $('#guessed').css('color','yellow');
+    $('#guessed').text('Erreur de saisie.');
+  } else {
+    if ($('#valueGuess').val() > guessMe) {
+      $('#guessed').css('background-color','red');
       $('#guessed').css('color','white');
-		$('#guessed').text('Moins.');
-  }else if ($('#valueGuess').val() < guessMe){
-    $('#guessed').css('background-color','red');
+      $('#guessed').text('Moins.');
+    }else if ($('#valueGuess').val() < guessMe){
+      $('#guessed').css('background-color','red');
       $('#guessed').css('color','white');
-  	$('#guessed').text('Plus.');
-  }else if ($('#valueGuess').val() == guessMe){
-    $('#guessed').css('background-color','lightgreen');
+      $('#guessed').text('Plus.');
+    }else if ($('#valueGuess').val() == guessMe){
+      $('#guessed').css('background-color','lightgreen');
       $('#guessed').css('color','black');
-  	$('#guessed').text('Correcte.');
+      $('#guessed').text('Correct. Vous avez réussi en '+nbrClick+' essais !');
+    }
   }
 });
